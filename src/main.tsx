8d7237be8eb1509436721949d192b63d0ac9f179
divineDuckk <assasinsmn@gmail.com>
2024-08-28 19:59:45 +0300

    feat: make theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,34 @@
+import { useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 
-import { persistor, store } from './store';
-
 import { App } from '@/components/App';
 
+import { persistor, store } from './store';
 import './firebase';
+import { getTheme } from './store/selectors/theme';
+import { useAppSelector } from './store/hooks';
 
 import './theme.scss';
+
+const Root = () => {
+  const theme = useAppSelector(getTheme);
+
+  useEffect(() => {
+    document.documentElement.dataset.theme = theme;
+    console.log(theme);
+  }, [theme]);
+
+  return <App />;
+};
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Provider store={store}>
       <PersistGate persistor={persistor} loading={null}>
-        <App />
+        <Root />
       </PersistGate>
     </Provider>
   </BrowserRouter>,
